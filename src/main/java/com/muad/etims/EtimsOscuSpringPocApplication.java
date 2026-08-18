876d7b680eb6b303ab628246144d41b55df0a552
JaMuad <43215483+JaMuad@users.noreply.github.com>
2026-08-19 01:18:34 +0300

    feat: initialize Spring Boot project structure with Maven wrapper and environment configuration

---
+++ src/main/java/com/muad/etims/EtimsOscuSpringPocApplication.java
@@ -0,0 +1,13 @@
+package com.muad.etims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtimsOscuSpringPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtimsOscuSpringPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muad/etims/EtimsOscuSpringPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muad.etims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtimsOscuSpringPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
